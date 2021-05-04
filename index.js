bc639e2fb539380b861768cef95ea3977619112d
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-05-04 22:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ let findEmployeeByFirstName = function(srcArray, firstName) {
       }, 0)
   }
 
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
